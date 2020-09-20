42d299a46248ba607fb59e678dfe10f1adea278a
FakDL <43212319+FakDL@users.noreply.github.com>
2020-09-20 20:18:04 +0300

    task 2 done

--- app/src/main/java/com/example/mobile_course/MainActivity.kt
+++ app/src/main/java/com/example/mobile_course/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.mobile_course
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.view.View
+import androidx.core.view.get
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+    fun toEdit(view: View){
+        my_switcher.showNext()
+        var name = et_name.text
+        tv_name.text = name
     }
 }
