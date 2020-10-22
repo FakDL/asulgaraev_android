c1f950c34dd0dbc54a2d27ec893e702e46e6644b
FakDL <43212319+FakDL@users.noreply.github.com>
2020-10-22 18:18:22 +0300

    recycler view

---
+++ app/src/main/java/com/example/mobile_course/data/UserData.kt
@@ -0,0 +1,13 @@
+package com.example.mobile_course.data
+
+import com.example.mobile_course.R
+import com.example.mobile_course.models.User
+
+object UserData {
+    val objects = arrayOf(
+        User(1, R.mipmap.ic_pic_3,"Paulo","He's very nice and has juul"),
+        User(2, R.mipmap.ic_profile_pic,"Carlos","Damn he's thicc"),
+        User(3, R.mipmap.ic_hogwarts, "Potter Cat", "Definitely gryffindor"),
+        User(4, R.mipmap.ic_fat, "Cowboy", "Nice hat, little boy")
+    )
+}
---
+++ app/src/main/java/com/example/mobile_course/models/User.kt
@@ -0,0 +1,8 @@
+package com.example.mobile_course.models
+
+data class User (
+    val id: Int,
+    val iv_id: Int,
+    val name: String,
+    val desc: String
+)
